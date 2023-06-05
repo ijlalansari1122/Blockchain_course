35905057abd0e8739f9b74e89b8fb6c5a2be5ac3
ijlalansari1122 <ansariijlal90@gmail.com>
2023-06-05 11:45:06 +0500

    week_1 in hashing

--- Nust_Blockchain/Week_1/app.py
+++ Nust_Blockchain/Week_1/app.py
@@ -3,41 +3,41 @@ import hashlib;
 
 # string
 
-Name_id = 'Ijlal ansari'
+Name_id = 'Ijlalansari'
 
 # convert to encode
 
-Convert_id=Name_id.encode()
+Convert_id = Name_id.encode()
 
 print(Convert_id)
 
 salt =bcrypt.gensalt();
-bcrypt_name = bcrypt.hashpw(Name_id.encode(), salt)
+bcrypt_name = bcrypt.hashpw(Convert_id, salt)
 #MD-5
 
 print(bcrypt_name)
 
-Blake2  = hashlib.blake2b(Name_id.encode()).hexdigest()
+Blake2  = hashlib.blake2s(Convert_id).hexdigest()
 #MD-5
 print(Blake2)
 
 
-md5_name  = hashlib.sha256(Name_id.encode()).hexdigest()
-#MD-5
+md5_name  = hashlib.sha256(Convert_id).hexdigest()
+# #MD-5
 print(md5_name)
 
-sha_512  = hashlib.sha512(Name_id.encode()).hexdigest()
+sha_512  = hashlib.sha512(Convert_id).hexdigest()
 #MD-5
 print(sha_512)
 
-sha_256  = hashlib.sha256(Name_id.encode()).hexdigest()
+sha_256  = hashlib.sha256(Convert_id).hexdigest()
 print(sha_256)
 
-Sha_1  = hashlib.sha1(Name_id.encode()).hexdigest()
+Sha_1  = hashlib.sha1(Convert_id).hexdigest()
 print(Sha_1)
 
 #sha-1
-ripemd160_Name = hashlib.new("ripemd160", Name_id.encode()).hexdigest()
+ripemd160_Name = hashlib.new("ripemd160", Convert_id).hexdigest()
 print(ripemd160_Name) 
 
  
@@ -56,57 +56,48 @@ print(ripemd160_Name)
 
 
 
-
-
-
-
+# 
+# 
+# 
+# 
 # string
-
-Name_id = 'Parvezlahi re-arrested from outside Lahore cout'
-
+# 
+Name_id = 'Parvez elahi re-arrested from outside Lahore court'
+# 
 # convert to encode
-
+# 
 Convert_id=Name_id.encode()
-
+# 
 print(Convert_id)
-
+# 
 salt =bcrypt.gensalt();
-bcrypt_name = bcrypt.hashpw(Name_id.encode(), salt)
+bcrypt_name = bcrypt.hashpw(Convert_id, salt)
 #MD-5
-
+# 
 print(bcrypt_name)
-
-Blake2  = hashlib.blake2b(Name_id.encode()).hexdigest()
+# 
+Blake2  = hashlib.blake2b(Convert_id).hexdigest()
 #MD-5
 print(Blake2)
-
-
-md5_name  = hashlib.sha256(Name_id.encode()).hexdigest()
+# 
+# 
+md5_name  = hashlib.sha256(Convert_id).hexdigest()
 #MD-5
 print(md5_name)
-
-sha_512  = hashlib.sha512(Name_id.encode()).hexdigest()
+# 
+sha_512  = hashlib.sha512(Convert_id).hexdigest()
 #MD-5
 print(sha_512)
-
-sha_256  = hashlib.sha256(Name_id.encode()).hexdigest()
+# 
+sha_256  = hashlib.sha256(Convert_id).hexdigest()
 print(sha_256)
-
-Sha_1  = hashlib.sha1(Name_id.encode()).hexdigest()
+# 
+Sha_1  = hashlib.sha1(Convert_id).hexdigest()
 print(Sha_1)
-
+# 
 #sha-1
-ripemd160_Name = hashlib.new("ripemd160", Name_id.encode()).hexdigest()
+ripemd160_Name = hashlib.new("ripemd160", Convert_id).hexdigest()
 print(ripemd160_Name) 
-
- 
-
-
-
-
-
-
- 
 
 
 
